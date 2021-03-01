fab3a642637c61405d28ec98fb51e0eee916f0ad
jerrylz <123>
2021-03-01 16:01:11 +0800

    add

---
+++ src/main/java/com/abchina/http/HttpConstant.java
@@ -0,0 +1,8 @@
+package com.abchina.http;
+
+/**
+ * @author jerrylz
+ * @date 2021/3/1
+ */
+public class HttpConstant {
+}
--- src/main/java/com/abchina/http/RequestWrapper.java
+++ src/main/java/com/abchina/http/RequestWrapper.java
@@ -1,376 +1,75 @@
 package com.abchina.http;
 
-import javax.servlet.AsyncContext;
-import javax.servlet.DispatcherType;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpUpgradeHandler;
-import javax.servlet.http.Part;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.Principal;
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.Locale;
+import cn.hutool.core.util.StrUtil;
+
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author jerrylz
- * @date 2021/2/27
+ * @date 2021/3/1
  */
-public abstract class RequestWrapper implements HttpServletRequest {
-
-    @Override
-    public String getAuthType() {
-        return null;
-    }
-
-    @Override
-    public Cookie[] getCookies() {
-        return new Cookie[0];
-    }
-
-    @Override
-    public long getDateHeader(String s) {
-        return 0;
-    }
-
-    @Override
-    public String getHeader(String s) {
-        return null;
-    }
-
-    @Override
-    public Enumeration<String> getHeaders(String s) {
-        return null;
-    }
-
-    @Override
-    public Enumeration<String> getHeaderNames() {
-        return null;
-    }
-
-    @Override
-    public int getIntHeader(String s) {
-        return 0;
-    }
-
-    @Override
-    public String getMethod() {
-        return null;
-    }
-
-    @Override
-    public String getPathInfo() {
-        return null;
-    }
-
-    @Override
-    public String getPathTranslated() {
-        return null;
-    }
-
-    @Override
-    public String getContextPath() {
-        return null;
-    }
-
-    @Override
-    public String getQueryString() {
-        return null;
-    }
-
-    @Override
-    public String getRemoteUser() {
-        return null;
-    }
-
-    @Override
-    public boolean isUserInRole(String s) {
-        return false;
-    }
-
-    @Override
-    public Principal getUserPrincipal() {
-        return null;
-    }
-
-    @Override
-    public String getRequestedSessionId() {
-        return null;
-    }
-
-    @Override
-    public String getRequestURI() {
-        return null;
-    }
-
-    @Override
-    public StringBuffer getRequestURL() {
-        return null;
-    }
-
-    @Override
-    public String getServletPath() {
-        return null;
-    }
-
-    @Override
-    public HttpSession getSession(boolean b) {
-        return null;
-    }
-
-    @Override
-    public HttpSession getSession() {
-        return null;
-    }
-
-    @Override
-    public String changeSessionId() {
-        return null;
-    }
-
-    @Override
-    public boolean isRequestedSessionIdValid() {
-        return false;
-    }
-
-    @Override
-    public boolean isRequestedSessionIdFromCookie() {
-        return false;
-    }
-
-    @Override
-    public boolean isRequestedSessionIdFromURL() {
-        return false;
-    }
-
-    @Override
-    public boolean isRequestedSessionIdFromUrl() {
-        return false;
-    }
-
-    @Override
-    public boolean authenticate(HttpServletResponse httpServletResponse) throws IOException, ServletException {
-        return false;
-    }
-
-    @Override
-    public void login(String s, String s1) throws ServletException {
-
-    }
-
-    @Override
-    public void logout() throws ServletException {
-
-    }
-
-    @Override
-    public Collection<Part> getParts() throws IOException, ServletException {
-        return null;
-    }
-
-    @Override
-    public Part getPart(String s) throws IOException, ServletException {
-        return null;
-    }
-
-    @Override
-    public <T extends HttpUpgradeHandler> T upgrade(Class<T> aClass) throws IOException, ServletException {
-        return null;
-    }
-
-    @Override
-    public Object getAttribute(String s) {
-        return null;
-    }
-
-    @Override
-    public Enumeration<String> getAttributeNames() {
-        return null;
-    }
-
-    @Override
-    public String getCharacterEncoding() {
-        return null;
-    }
-
-    @Override
-    public void setCharacterEncoding(String s) throws UnsupportedEncodingException {
-
-    }
-
-    @Override
-    public int getContentLength() {
-        return 0;
-    }
-
-    @Override
-    public long getContentLengthLong() {
-        return 0;
-    }
-
-    @Override
-    public String getContentType() {
-        return null;
-    }
-
-    @Override
-    public ServletInputStream getInputStream() throws IOException {
-        return null;
-    }
-
-    @Override
-    public String getParameter(String s) {
-        return null;
-    }
-
-    @Override
-    public Enumeration<String> getParameterNames() {
-        return null;
-    }
-
-    @Override
-    public String[] getParameterValues(String s) {
-        return new String[0];
-    }
-
-    @Override
-    public Map<String, String[]> getParameterMap() {
-        return null;
-    }
-
-    @Override
-    public String getProtocol() {
-        return null;
-    }
-
-    @Override
-    public String getScheme() {
-        return null;
-    }
-
-    @Override
-    public String getServerName() {
-        return null;
-    }
-
-    @Override
-    public int getServerPort() {
-        return 0;
-    }
-
-    @Override
-    public BufferedReader getReader() throws IOException {
-        return null;
-    }
-
-    @Override
-    public String getRemoteAddr() {
-        return null;
-    }
-
-    @Override
-    public String getRemoteHost() {
-        return null;
-    }
-
-    @Override
-    public void setAttribute(String s, Object o) {
-
-    }
-
-    @Override
-    public void removeAttribute(String s) {
-
-    }
-
-    @Override
-    public Locale getLocale() {
-        return null;
-    }
-
-    @Override
-    public Enumeration<Locale> getLocales() {
-        return null;
-    }
-
-    @Override
-    public boolean isSecure() {
-        return false;
-    }
-
-    @Override
-    public RequestDispatcher getRequestDispatcher(String s) {
-        return null;
-    }
-
-    @Override
-    public String getRealPath(String s) {
-        return null;
-    }
-
-    @Override
-    public int getRemotePort() {
-        return 0;
-    }
-
-    @Override
-    public String getLocalName() {
-        return null;
-    }
-
-    @Override
-    public String getLocalAddr() {
-        return null;
-    }
-
-    @Override
-    public int getLocalPort() {
-        return 0;
-    }
-
-    @Override
-    public ServletContext getServletContext() {
-        return null;
-    }
-
-    @Override
-    public AsyncContext startAsync() throws IllegalStateException {
-        return null;
-    }
-
-    @Override
-    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) throws IllegalStateException {
-        return null;
-    }
-
-    @Override
-    public boolean isAsyncStarted() {
-        return false;
-    }
-
-    @Override
-    public boolean isAsyncSupported() {
-        return false;
-    }
-
-    @Override
-    public AsyncContext getAsyncContext() {
-        return null;
-    }
-
-    @Override
-    public DispatcherType getDispatcherType() {
-        return null;
+public class HttpWrapper {
+
+    private static final String CRLF = "\r\n";
+    private static final String SP = " ";
+    private static final String HEADER_SP = ":";
+    //请求行
+    private Map<String, String> requestLineMap = new HashMap<>();
+    //请求头
+    private Map<String, String> requestHeaderMap = new HashMap<>();
+    //参数
+    private Map<String, List<String>> parameterMap = new HashMap<>();
+
+
+    public void parseRequestInfo(String requestInfo){
+        if(StrUtil.isBlank(requestInfo)){
+            return;
+        }
+
+        String[] reqArray = StrUtil.split(requestInfo, CRLF);
+
+        for(int i = 0; i < reqArray.length; i++){
+            if(i == 0){
+                //解析请求行
+                parseRequestLine(reqArray[i]);
+            }else{
+                if(StrUtil.isNotBlank(reqArray[i])){
+                    //非空白行，则解析请求头
+                    parseRequestHeader(reqArray[i]);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 解析请求行
+     * @param requestLine
+     */
+    private void parseRequestLine(String requestLine) {
+        //请求行格式GET /do HTTP/1.1
+        //以空格分割
+        String[] lineArray = StrUtil.split(requestLine, SP);
+        for(int i = 0; i < lineArray.length; i++){
+            if(i == 0){
+                this.requestLineMap.put("method",lineArray[0].trim());
+            }else if(i == 1){
+                this.requestLineMap.put("url", lineArray[1].trim());
+            }else if(i == 2){
+                this.requestLineMap.put("httpVersion", lineArray[2].trim());
+            }
+        }
+    }
+
+    /**
+     * 解析请求头数据
+     * @param requestHeader
+     */
+    private void parseRequestHeader(String requestHeader){
+        //请求头格式 XXX:XXX
+        String[] headerArray = StrUtil.split(requestHeader, HEADER_SP);
+        this.requestHeaderMap.put(headerArray[0], headerArray[1]);
     }
 }
